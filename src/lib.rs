6cf34f276ede142b7553529bc3fba141d5fdc965
hridampalwe <palwehridam@gmail.com>
2023-01-29 20:56:27 +0530

    Add parameter for case-insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub struct Config
 {
     pub query : String,
     pub file_path : String,
+    pub ignore_case : bool,
 }
 
 impl Config
@@ -15,16 +16,35 @@ impl Config
         {
             return Err("Not enough arguments");
         }
+        let ic = match args.get(3) 
+        {
+            Some(s) => {
+                let val = if s == "-ic" {
+                    true
+                }
+                else {
+                    false
+                };
+                val
+            },
+            None => false,
+        };
         let t = Config{
             query : args[1].clone(),
             file_path : args[2].clone(),
+            ignore_case : ic,
         };
         Ok(t)
     }
 }
 pub fn run(config: Config) -> Result<() , Box<dyn Error>> {
     let file_in_str = fs::read_to_string(config.file_path)?;
-    let lines_found = search(&file_in_str , &config.query);
+    let lines_found = if config.ignore_case{
+        search_ic(&file_in_str , &config.query)
+    }
+    else{
+        search(&file_in_str , &config.query)
+    };
     for line in lines_found
     {
         println!("{line}");
@@ -38,6 +58,19 @@ pub fn search<'a>(content : &'a str , query : &str) -> Vec<&'a str>
     for line in content.lines()
     {
         if line.contains(query)
+        {
+            lines_found.push(line);
+        }
+    }
+    lines_found
+}
+pub fn search_ic<'a>(content : &'a str , query : &str) -> Vec<&'a str> 
+{
+    let mut lines_found : Vec<&str> = Vec::new();
+    let query = query.to_lowercase();
+    for line in content.lines()
+    {
+        if line.to_lowercase().contains(&query)
         {
             lines_found.push(line);
         }
